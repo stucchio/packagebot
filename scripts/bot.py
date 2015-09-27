5645fba8d29bbb67214d173ada17ce21ad28dd38
Chris Stucchio <stucchio@gmail.com>
2015-09-27 09:21:55 -0400

    bot now reads durations from config

--- scripts/bot.py
+++ scripts/bot.py
@@ -1,10 +1,10 @@
 import packagebot.interact as interact
+import packagebot.config as config
 import time
 import logging
 
-logging.basicConfig(format='%(asctime)-15s -8s %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)-15s -8s %(message)s', level=logging.DEBUG)
 
 while True:
-    interact.pull_updates()
+    interact.pull_updates(config.TELEGRAM_POLL_TIMEOUT, config.TELEGRAM_POLL_LIMIT)
     interact.handle_old_requests()
-    time.sleep(1)
